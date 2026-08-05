0d95cc57d6cbdd7c179dbbcba422566d0a0c9349
Ophidian123 <nemanipavan12@gmail.com>
2026-08-05 11:43:18 -0700

    Add files via upload

---
+++ src/client/java/com/chlorine/client/ChlorineClient.java
@@ -0,0 +1,33 @@
+package com.chlorine.client;
+
+import com.chlorine.Chlorine;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+
+public class ChlorineClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        PerformanceScaler scaler = new PerformanceScaler();
+        PowerSaver powerSaver = new PowerSaver();
+        // Wrapped in an array since it needs to be mutated from inside the
+        // tick lambda below (effectively-final capture won't allow a plain
+        // boolean local here).
+        boolean[] autoTuneApplied = {false};
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (!autoTuneApplied[0]) {
+                LowEndAutoTune.applyIfNeeded(client);
+                autoTuneApplied[0] = true;
+            }
+            if (Chlorine.CONFIG.enableAdaptiveSimulationDistance) {
+                scaler.tick(client);
+            }
+            if (Chlorine.CONFIG.enablePowerSaver) {
+                powerSaver.tick(client);
+            }
+        });
+
+        Chlorine.LOGGER.info("Chlorine client-side laptop tuning active");
+    }
+}
